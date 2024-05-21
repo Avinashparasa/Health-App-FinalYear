b900828221e20791d674b3a61599b0acc2cabd70
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-05-21 17:00:17 +0530

    tailwind setup

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header>
       <div>
-        <h1>Fitness Planner</h1>
+        <Link to={"/"}>
+          <h1>Fitness Planner</h1>
+        </Link>
       </div>
     </header>
   );
