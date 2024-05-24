5558cd0dc4889ad1379be945850d80f68556e032
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-05-24 16:55:30 +0530

    updated landing page and add constant file

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { MainPageBg } from "../utils/constants";
 
 const LandingPage = () => {
-  return <div>landing page</div>;
+  return (
+    <div className="relative">
+      <img
+        src={MainPageBg}
+        alt="landingPageBackgroundImage"
+        className="w-full h-auto"
+      />
+      <div className="absolute inset-0 flex items-center justify-center flex-col">
+        <h1 className="text-white font-bold text-4xl bg-black bg-opacity-50 p-4 rounded">
+          Get Result You Can See And Feel.
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const MainPageBg =
+  "https://images.unsplash.com/photo-1554284126-aa88f22d8b74?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
