79728fefdcc50473819fff0d3c32c7b91eeb0d2a
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-05-25 23:41:17 +0530

    login form setup

--- frontend/src/pages/UserRegister.js
+++ frontend/src/pages/UserRegister.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const userRegister = () => {
+const UserRegister = () => {
+  const [isLogInForm, setIsLogInForm] = useState(true);
+  const signUPForm = () => {
+    setIsLogInForm(!isLogInForm);
+  };
   return (
     <div className="my-10 flex justify-center">
-      <form className="md:w-1/2 w-10/12 my-5">
+      <form
+        className="md:w-1/2 w-10/12 my-5"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h3 className="font-bold text-2xl text-blue-700 md:pt-10 mb-5">
-          Sign Up
+          {isLogInForm ? "Sign In" : "Sign Up"}
         </h3>
-        <div className="mb-2">
-          <label className="text-blue-400 font-semibold  block mb-2">
-            User Name
-          </label>
-          <input
-            type="text"
-            className="w-full md:w-10/12 h-10 outline outline-1 rounded outline-gray-200"
-          />
-        </div>
+        {!isLogInForm && (
+          <div className="mb-2">
+            <label className="text-blue-400 font-semibold  block mb-2">
+              User Name
+            </label>
+            <input
+              type="text"
+              className="w-full md:w-10/12 h-10 outline outline-1 rounded outline-gray-200"
+            />
+          </div>
+        )}
         <div className="mb-2">
           <label className="text-blue-400 font-semibold  block mb-2">
             Email
@@ -34,10 +43,21 @@ const userRegister = () => {
             className="w-full md:w-10/12 h-10 outline outline-1 rounded outline-gray-200"
           />
         </div>
-        <button className="bg-blue-700 text-white p-2 rounded">Sign Up</button>
+        <button className="bg-blue-700 text-white p-2 rounded mb-2">
+          {isLogInForm ? "Sign In" : "Sign Up"}
+        </button>
+
+        <p
+          className="text-blue-400 font-semibold mb-2 cursor-pointer"
+          onClick={signUPForm}
+        >
+          {isLogInForm
+            ? "New to Fitness Planner? Sign up now."
+            : "Already registered? Sign In Now."}
+        </p>
       </form>
     </div>
   );
 };
 
-export default userRegister;
+export default UserRegister;
