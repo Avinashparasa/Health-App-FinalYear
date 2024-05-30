a2df55da5b27c75bff32b021ce3616bd5ee595ac
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-05-31 02:13:37 +0530

    modify requireAuth file

--- Backend/middleware/requireAuth.js
+++ Backend/middleware/requireAuth.js
@@ -2,9 +2,10 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const requireAuth = async (req, res, next) => {
-  const token = req.cookies.jwt;
+  const authHeader = req.headers.authorization;
 
-  if (token) {
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
     try {
       const decoded = jwt.verify(token, process.env.JWT_secret);
 
