51731dc66116f398f19ccfbc5c3fc79baa6bf3df
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-05-30 21:31:13 +0530

    seting up cors

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
@@ -11,6 +12,7 @@ const app = express();
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log("path : " + req.path);
